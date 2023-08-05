61f0cef2206dd01754b62eddf7cc550d79a94d28
jiangzhihong <601079657@qq.com>
2023-08-05 15:46:44 +0800

    创建项目

---
+++ src/main/java/com/jiangzhihong/java/easydemo/EasyDemoApplication.java
@@ -0,0 +1,19 @@
+package com.jiangzhihong.java.easydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyDemoApplication {
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(EasyDemoApplication.class, args);
+        } catch (Exception e) {
+            System.out.println("(￣^￣)ゞ报告！出现错误了！");
+            System.out.println(e);
+        }
+        System.out.println("乁( ˙ ω˙乁)项目启动了耶，它能跑我就不用跑了咯。");
+    }
+
+}
---
+++ src/test/java/com/jiangzhihong/java/easydemo/EasyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiangzhihong.java.easydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
