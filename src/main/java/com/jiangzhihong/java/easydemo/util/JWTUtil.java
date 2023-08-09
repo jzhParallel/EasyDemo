1dc396a4da58161732cb743bfbb100b785da2243
jiangzhihong <601079657@qq.com>
2023-08-09 17:22:42 +0800

    新建了jwt工具类

---
+++ src/main/java/com/jiangzhihong/java/easydemo/util/JWTUtil.java
@@ -0,0 +1,46 @@
+package com.jiangzhihong.java.easydemo.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @program: EasyDemo
+ * @description: jwt工具类
+ * @author: jiangzhihong
+ * @create: 2023-08-09 17:12
+ **/
+public class JWTUtil {
+    private static Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public static String createToken(Integer uid) {
+        Map<String, Object> chain = new HashMap<String, Object>();
+        chain.put("userId", uid);
+        String token = Jwts.builder()
+                //.setSubject("用于认证的token")//设置主题
+                .signWith(key)//签发算法与密钥jwtToken
+                .setClaims(chain)//唯一的body数据
+                .setIssuedAt(new Date())//签发时间
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 10))//10天有效期
+                .compact();
+        return token;
+    }
+
+    public static Claims checkToken(String token) {
+        try {
+            Jwt parse = Jwts.parserBuilder()
+                    .setSigningKey(key).build().parseClaimsJws(token);
+            return (Claims) parse.getBody();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+}
