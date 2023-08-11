bc6c80e142bc3551e42fdd7323cd6780dca6efbb
jiangzhihong <601079657@qq.com>
2023-08-11 15:10:07 +0800

    使用拦截器检查请求头中是否存在合法token

---
+++ src/main/java/com/jiangzhihong/java/easydemo/handle/LoginInterceptor.java
@@ -0,0 +1,45 @@
+package com.jiangzhihong.java.easydemo.handle;
+
+import com.jiangzhihong.java.easydemo.mapper.UserMapper;
+import com.jiangzhihong.java.easydemo.util.JWTUtil;
+import io.jsonwebtoken.Claims;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @program: EasyDemo
+ * @description: 登录拦截器
+ * @author: jiangzhihong
+ * @create: 2023-08-11 14:25
+ **/
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("Authorization");
+        // 验证Token的有效性
+        if (token == null) return false;
+        Claims checkedToken = JWTUtil.checkToken(token);
+        int uid = (int) checkedToken.get("userId");
+        if (userMapper.selectByUid(uid) == null) return false;
+        System.out.print("已通过拦截器...");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        // 在请求完成后执行的代码
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // 在请求完成后执行的代码
+    }
+}
